c0bfea5892507df0d685f69555063fec755b3217
Kevin Xu <kvnlxu@gmail.com>
2019-05-09 08:46:23 -0700

    added snapshot test

--- src/composition/Card.test.js
+++ src/composition/Card.test.js
@@ -1,19 +1,26 @@
-// make React available
 import React from 'react';
-
-//make the ReactDOM available, necessary for rendering the component
 import ReactDOM from 'react-dom';
-
 import Card from './Card';
+import renderer from 'react-test-renderer';
 
-//this is the test case
-it('renders without crashing', () => {
-  // first create a DOM element to render the component into
-  const div = document.createElement('div');
+describe('Card', () => {
 
-  //render the component, this is the actual test, if something is wrong it will fail here
-  ReactDOM.render(<Card title="foo" content="bar"/>, div);
+  it('renders without crashing', () => {
+    // first create a DOM element to render the component into
+    const div = document.createElement('div');
 
-  //clean up code
-  ReactDOM.unmountComponentAtNode(div);
+    //render the component, this is the actual test, if something is wrong it will fail here
+    ReactDOM.render(<Card title="foo" content="bar"/>, div);
+
+    //clean up code
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<Card title="foo" content="bar"/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+
 });
--- src/composition/List.test.js
+++ src/composition/List.test.js
@@ -1,19 +1,26 @@
-// make React available
 import React from 'react';
-
-//make the ReactDOM available, necessary for rendering the component
 import ReactDOM from 'react-dom';
-
 import List from './List';
+import renderer from 'react-test-renderer';
 
-//this is the test case
-it('renders without crashing', () => {
-  // first create a DOM element to render the component into
-  const div = document.createElement('div');
+describe('List', () => {
 
-  //render the component, this is the actual test, if something is wrong it will fail here
-  ReactDOM.render(<List header="foo" cards={[]}/>, div);
+  it('renders without crashing', () => {
+    // first create a DOM element to render the component into
+    const div = document.createElement('div');
 
-  //clean up code
-  ReactDOM.unmountComponentAtNode(div);
+    //render the component, this is the actual test, if something is wrong it will fail here
+    ReactDOM.render(<List header="foo" cards={[]}/>, div);
+
+    //clean up code
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List header="foo" cards={[]}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+
 });
